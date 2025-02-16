e27923950dc9e2aa793469cb23dcf2b8952bac0b
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-16 15:27:55 +0100

    dummy call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import Navigation from '@/components/Navigation'
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY
+
+const test = async () => {
+    const response = await fetch(
+        'https://api.clashofclans.com/v1/players/%23RGC9YYGQ',
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${apiKey}`,
+            },
+        }
+    )
+
+    const result = await response.json()
+}
+
 export default function Home() {
-    return <div className="bg-red-500">Hello World!</div>
+    test()
+    return (
+        <div className="w-full max-w-screen-2xl uppercase">
+            <Navigation />
+        </div>
+    )
 }
