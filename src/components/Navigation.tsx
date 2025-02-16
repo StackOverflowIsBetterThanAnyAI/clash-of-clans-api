f1782cd08ce803fbec57003cc05bd1779e755110
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-16 19:09:00 +0100

    uppercase

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,7 +7,7 @@ const Navigation = () => {
         <nav className="flex justify-between items-center bg-zinc-300 dark:bg-zinc-700 text-zinc-800 dark:text-zinc-100 px-4 py-2 shadow-xl w-full">
             <a
                 href="/"
-                className="flex items-center gap-2 pr-2 py-1 hover:bg-zinc-100 hover:dark:bg-zinc-600 hover:shadow-md rounded-md"
+                className="flex items-center gap-2 uppercase pr-2 py-1 hover:bg-zinc-100 hover:dark:bg-zinc-600 hover:shadow-md rounded-md"
             >
                 <Image
                     alt="Back to the Homepage."
