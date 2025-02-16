486c03facb885de0678e86579f4b71e21183356c
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-16 19:33:15 +0100

    remove now redundant text classes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle'
 
 const Navigation = () => {
     return (
-        <nav className="flex justify-between items-center bg-zinc-300 dark:bg-zinc-700 text-zinc-800 dark:text-zinc-100 px-4 py-2 shadow-xl w-full">
+        <nav className="flex justify-between items-center bg-zinc-300 dark:bg-zinc-700 px-4 py-2 shadow-xl w-full">
             <a
                 href="/"
                 className="flex items-center gap-2 uppercase pr-2 py-1 hover:bg-zinc-100 hover:dark:bg-zinc-600 hover:shadow-md rounded-md"
