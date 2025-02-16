095429dd3faa509e673a22fc1f4fd6a9aab92143
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-16 18:55:53 +0100

    better looking; hover effects

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,13 +4,16 @@ import ThemeToggle from './ThemeToggle'
 
 const Navigation = () => {
     return (
-        <nav className="flex justify-between items-center bg-zinc-300 dark:bg-zinc-700 text-zinc-800 dark:text-zinc-100 px-4 pt-1 pb-2 shadow-lg w-full">
-            <a href="/" className="flex items-center gap-2">
+        <nav className="flex justify-between items-center bg-zinc-300 dark:bg-zinc-700 text-zinc-800 dark:text-zinc-100 px-4 py-2 shadow-xl w-full">
+            <a
+                href="/"
+                className="flex items-center gap-2 pr-2 py-1 hover:bg-zinc-100 hover:dark:bg-zinc-600 hover:shadow-md rounded-md"
+            >
                 <Image
                     alt="Back to the Homepage."
                     src={logo}
                     className="object-contain"
-                    height={64}
+                    height={48}
                     priority
                 />
                 <h1 className="text-lg sm:text-xl pt-2">Clash Stats</h1>
