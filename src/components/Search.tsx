c4a9cb7cb37ec4fb328cb560768ae3d69e457998
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-17 21:33:55 +0100

    search logic

---
+++ src/components/Search.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import { ContextPlayer } from '@/app/page'
+import { setItemInStorage } from '@/app/utils/setItemInStorage'
+import { useContext, useEffect, useState } from 'react'
+
+const Search = () => {
+    const contextPlayer = useContext(ContextPlayer)
+    if (!contextPlayer) {
+        throw new Error(
+            'ContextPlayer must be used within a ContextPlayer.Provider'
+        )
+    }
+    const [player, setPlayer] = contextPlayer
+    const [playerID, setPlayerID] = useState<string>('')
+    const [isDisabled, setIsDisabled] = useState<boolean>(false)
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const storedData = sessionStorage.getItem('clash-of-clans')
+            if (storedData) {
+                try {
+                    const parsedData = JSON.parse(storedData)
+                    setPlayerID(parsedData.id || '')
+                    setIsDisabled(!parsedData.id)
+                } catch (error) {
+                    console.error(error)
+                }
+            }
+        }
+    }, [])
+
+    const fetchPlayer = async (id: string) => {
+        try {
+            setIsDisabled(true)
+            setPlayer({
+                ...player,
+                name: '',
+                expLevel: '',
+                townHallLevel: '',
+                clanTag: '',
+                clanName: '',
+                clanBadgeUrl: '',
+                trophies: '',
+                builderBaseTrophies: '',
+            })
+
+            const response = await fetch(`/api/player?id=${id.substring(1)}`)
+            if (response.status !== 200) {
+                throw new Error('Unable to fetch the current user!')
+            }
+            const data = await response.json()
+
+            setItemInStorage({
+                id: playerID,
+                name: data.name,
+                expLevel: data.expLevel,
+                townHallLevel: data.townHallLevel,
+                clanTag: data.clan.tag,
+                clanName: data.clan.name,
+                clanBadgeUrl: data.clan.badgeUrls.medium,
+                trophies: data.trophies,
+                builderBaseTrophies: data.builderBaseTrophies,
+            })
+            setPlayer({
+                ...player,
+                name: data.name,
+                expLevel: data.expLevel,
+                townHallLevel: data.townHallLevel,
+                clanTag: data.clan.tag,
+                clanName: data.clan.name,
+                clanBadgeUrl: data.clan.badgeUrls.medium,
+                trophies: data.trophies,
+                builderBaseTrophies: data.builderBaseTrophies,
+            })
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsDisabled(false)
+        }
+    }
+
+    const handleSearch = (id: string) => {
+        fetchPlayer(id)
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const pass = /^#[a-z0-9]{8}$/i.test(e.target.value)
+        setIsDisabled(!pass)
+        setPlayerID(e.target.value.toUpperCase())
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && !isDisabled) handleSearch(playerID)
+    }
+
+    return (
+        <>
+            <label
+                className="text-sm sm:text-base text-center pt-2"
+                htmlFor="search"
+            >
+                Enter Player ID
+            </label>
+            <input
+                id="search"
+                type="search"
+                onChange={(e) => handleChange(e)}
+                onKeyDown={(e) => handleKeyDown(e)}
+                className="text-sm sm:text-base m-auto max-w-96 outline outline-2 outline-zinc-700 dark:outline-zinc-900 px-2 py-1 my-1 rounded-md w-full"
+                placeholder="#RGC9YYGQ"
+                autoCapitalize="true"
+                autoComplete="on"
+                maxLength={9}
+                value={playerID}
+            />
+            <button
+                onClick={() => handleSearch(playerID)}
+                className="mx-auto text-xs sm:text-sm bg-zinc-100 shadow-md dark:bg-zinc-800 rounded-md px-2 py-1 mt-1 w-fit"
+                disabled={isDisabled}
+            >
+                Search
+            </button>
+        </>
+    )
+}
+
+export default Search
