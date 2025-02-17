9769159c7fbf9cdcd1ebe42dc7d99cd323065d18
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-17 21:34:27 +0100

    context variable for current player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
+'use client'
+
+import Main from '@/components/Main'
 import Navigation from '@/components/Navigation'
 import Search from '@/components/Search'
+import { PlayerProps } from '@/types/PlayerProps'
+import { createContext, Dispatch, SetStateAction, useState } from 'react'
+
+export const ContextPlayer = createContext<
+    [PlayerProps, Dispatch<SetStateAction<PlayerProps>>] | undefined
+>(undefined)
 
 const Home = () => {
+    const [player, setPlayer] = useState<PlayerProps>({
+        id: '',
+        name: '',
+        expLevel: '',
+        townHallLevel: '',
+        clanTag: '',
+        clanName: '',
+        clanBadgeUrl: '',
+        trophies: '',
+        builderBaseTrophies: '',
+    })
+
     return (
         <div className="flex flex-col w-full max-w-screen-2xl">
-            <Navigation />
-            <Search />
+            <ContextPlayer.Provider value={[player, setPlayer]}>
+                <Navigation />
+                <Search />
+                <Main />
+            </ContextPlayer.Provider>
         </div>
     )
 }
