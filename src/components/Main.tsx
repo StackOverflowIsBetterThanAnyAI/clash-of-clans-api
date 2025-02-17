0a4cf0d953679c78e2f56f44bfb1d0077252cd48
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-17 21:34:06 +0100

    main content

---
+++ src/components/Main.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { ContextPlayer } from '@/app/page'
+import { useContext, useEffect } from 'react'
+import Image from 'next/image'
+import barbarian from '@/assets/barbarian.png'
+import king from '@/assets/king.png'
+
+const Main = () => {
+    const contextPlayer = useContext(ContextPlayer)
+    if (!contextPlayer) {
+        throw new Error(
+            'ContextPlayer must be used within a ContextPlayer.Provider'
+        )
+    }
+    const [player, setPlayer] = contextPlayer
+
+    useEffect(() => {
+        const storage = JSON.parse(sessionStorage.getItem('clash-of-clans')!)
+        setPlayer({
+            ...player,
+            name: storage?.name,
+            expLevel: storage?.expLevel,
+            townHallLevel: storage?.townHallLevel,
+            clanTag: storage?.clanTag,
+            clanName: storage?.clanName,
+            clanBadgeUrl: storage?.clanBadgeUrl,
+            trophies: storage?.trophies,
+            builderBaseTrophies: storage?.builderBaseTrophies,
+        })
+    }, [])
+
+    return (
+        <main className="flex justify-center items-center flex-wrap gap-3 bg-zinc-300 dark:bg-zinc-500 shadow-lg mt-2 px-2 py-4 w-full">
+            {player.name && (
+                <>
+                    <Image
+                        src={king}
+                        alt=""
+                        height={256}
+                        className="drop-shadow-lg hidden lg:block"
+                    />
+                    <div className="flex flex-col">
+                        <p className="flex justify-center text-xl sm:text-2xl pb-2">
+                            {player.name}
+                        </p>
+                        <div className="flex flex-wrap text-sm sm:text-base items-center justify-center">
+                            Player Level{' '}
+                            <p className="bg-blue-500 text-zinc-100 outline outline-2 outline-zinc-100 m-2 p-1 rounded-md">
+                                {player.expLevel}
+                            </p>{' '}
+                            Townhall Level{' '}
+                            <p className="bg-red-600 text-zinc-100 outline outline-2 outline-zinc-100 m-2 p-1 rounded-md">
+                                {player.townHallLevel}
+                            </p>
+                        </div>
+                        <div className="flex flex-wrap text-sm sm:text-base items-center justify-center">
+                            Trophies{' '}
+                            <p className="bg-purple-600 text-zinc-100 outline outline-2 outline-zinc-100 m-2 p-1 rounded-md">
+                                {player.trophies}
+                            </p>{' '}
+                            Builder Base Trophies{' '}
+                            <p className="bg-green-600 text-zinc-100 outline outline-2 outline-zinc-100 m-2 p-1 rounded-md">
+                                {player.builderBaseTrophies}
+                            </p>
+                        </div>
+                        <p className="flex flex-wrap gap-2 text-sm sm:text-base items-center justify-center">
+                            {player.clanName}
+                            <Image
+                                src={player.clanBadgeUrl}
+                                alt={player.clanName}
+                                height={48}
+                                width={48}
+                            />
+                            {player.clanTag}
+                        </p>
+                    </div>
+                    <Image
+                        src={barbarian}
+                        alt=""
+                        height={256}
+                        className="scale-x-[-1] drop-shadow-lg h-48 sm:h-64 object-contain"
+                    />
+                </>
+            )}
+        </main>
+    )
+}
+
+export default Main
