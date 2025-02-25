5da22e83d144a9144ee9b3a3eed53b3db160382a
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-25 20:37:50 +0100

    test id

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,10 @@ import ThemeToggle from './ThemeToggle'
 
 const Navigation = () => {
     return (
-        <nav className="flex justify-between items-center bg-zinc-300 dark:bg-zinc-700 px-4 py-2 shadow-xl w-full">
+        <nav
+            className="flex justify-between items-center bg-zinc-300 dark:bg-zinc-700 px-4 py-2 shadow-xl w-full"
+            data-testid="navigation"
+        >
             <a
                 href="/"
                 className="flex items-center gap-2 uppercase pr-2 py-1 hover:bg-zinc-100 hover:dark:bg-zinc-600 hover:shadow-md rounded-md"
@@ -16,6 +19,7 @@ const Navigation = () => {
                     className="object-contain"
                     height={48}
                     priority
+                    data-testid="navigation-img"
                 />
                 <h1
                     className="text-lg sm:text-xl pt-2"
