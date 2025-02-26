4541e6fd5cabf0267055337595ed862c53d69a29
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-26 21:08:36 +0100

    intercept

--- cypress/e2e/clash-of-clans/search.cy.js
+++ cypress/e2e/clash-of-clans/search.cy.js
@@ -4,20 +4,26 @@ context('Search', () => {
     beforeEach(() => {
         cy.visit('/')
         cy.clearLocalStorage()
-
-        cy.intercept('GET', '/api/player', (req) => {
-            req.headers['Authorization'] = `Bearer ${Cypress.env('API_TOKEN')}`
-        }).as('searchRequest')
     })
 
     afterEach(() => {
         cy.clearLocalStorage()
     })
 
+    it('should exist api key', () => {
+        const apiKey = Cypress.env('API_KEY')
+        expect(apiKey).to.exist
+    })
+
     it('should display user data', () => {
+        const apiKey = Cypress.env('API_KEY')
         cy.get('[data-testid="search-input"]').type('#rgc9yygq')
+
+        cy.intercept('GET', '/api/player?id=RGC9YYGQ', (req) => {
+            req.headers['Authorization'] = `Bearer ${apiKey}`
+        })
+
         cy.get('[data-testid="search-button"]').click()
-        cy.wait('@searchRequest').its('response.statusCode').should('eq', 200)
         cy.get('[data-testid="main-player"]').should(
             'contain.text',
             '0 SKLZ,JUS LUCK'
@@ -25,9 +31,14 @@ context('Search', () => {
     })
 
     it('should display error instead of user data', () => {
+        const apiKey = Cypress.env('API_KEY')
         cy.get('[data-testid="search-input"]').type('#12345678')
+
+        cy.intercept('POST', '/api/player?id=RGC9YYGQ', (req) => {
+            req.headers['Authorization'] = `Bearer ${apiKey}`
+        })
+
         cy.get('[data-testid="search-button"]').click()
-        cy.wait('@searchRequest')
         cy.get('[data-testid="search-error"]').should(
             'have.text',
             'Unable to fetch the player with ID #12345678!'
